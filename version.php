6dbf5d5ff66248e4d8a761a74e8487af04c38e4d
Yoshikazu Asada <yasada0819@users.noreply.github.com>
2025-12-11 22:19:36 +0900

    Update version.php

--- version.php
+++ version.php
@@ -5,4 +5,4 @@ $plugin->component = 'local_completionoverride';
 $plugin->version = 2025120900;
 $plugin->requires = 2022041900; // Moodle 4.0+
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0';
+$plugin->release = '0.5';
